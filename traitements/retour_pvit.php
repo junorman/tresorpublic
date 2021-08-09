017a376299a3754d1871ba2f2f237df25c99fa64
S@d <t.sadathe@gmail.com>
2021-08-09 21:51:09 +0100

    ok

--- traitements/retour_pvit.php
+++ traitements/retour_pvit.php
@@ -1,13 +1,18 @@
 <?php 
+include '../db/db.php';
 
 //traitement retour pvit......
 
+$data_received=file_get_contents("php://input");
+$data_received_xml=new SimpleXMLElement($data_received);
+$ligne_response=$data_received_xml[0];
 
+$reference_received=$ligne_response->REF;
+$statut_received=$ligne_response->STATUT;
+$operateur_received=$ligne_response->OPERATEUR;
+$client_received=$ligne_response->TEL_CLIENT;
+$message_received=$ligne_response->MESSAGE;
 
 $retour = '200';
 
-if($retour = '200'){
-    //generer facture ici
-}
-
-?>
+mysqli_query($db,"UPDATE tmp_paiement SET statut=".$retour." WHERE code='".$reference_received."'");
