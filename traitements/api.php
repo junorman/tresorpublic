d120085caeae144a46ca33318c1855003b56be6b
junior <roocky@90gmail.com>
2021-08-10 15:47:59 +0200

    ok

--- traitements/api.php
+++ traitements/api.php
@@ -1,19 +1,25 @@
 <?php 
 include '../db/db.php';
 
-$numero = $_POST['numero'];
-$message = $_POST['message'];
+$numero = $_POST['numero']="074820870";
+$message = $_POST['message']="hdoeho5989dzdz";
+echo $numero." ".$message;
 $date = date('Y-m-d H:i:s');
 $status = 0;
 $transaction = rand(15,35).$numero;
 $token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.d1gyRFhqNm1BWWFFUEc2YndkSkdOSWRvZkV1Um1vSG5yR3pISkhEa1EvdGQ4MFpQMXdGSGcxK0RrT2QvVU9yVzlHQWJ6dC9EbkhLd2E4a3FFV2dnU0JGbWpIc2ZLVjlLNUlKenBLNENKcWdBMjRrUW9YdUV3aElxTWhUWThySEhKcVlEWHpJRGdYdmJqV1VkQVRUQVVEVTBZa3hHQS9uMkMrNng2NlBSbjl5c2NqS2RlM0lsN0lWdjgzemZ2bDJYOjpHN3lpNXhjOVRZdG1tTVVTamJMcDN3PT0=.Yo8sFgnsTGMdUCrPQp4px34+N9j3PyM1gL7R1PRqJUY=";
 
-$sql = "SELECT * FROM titres WHERE code = '".$message."'"; 
+$sql = "SELECT * FROM titres WHERE code = '".$message."' AND statut = 0"; 
 $result = mysqli_query($db,$sql);
 $row = mysqli_num_rows($result);
 
 
 if ($row > 0) {
+
+	$sql2 = "SELECT * FROM titres WHERE code = '".$message."' AND statut=0"; 
+    $result2 = mysqli_query($db,$sql2);
+    $get_infos = mysqli_fetch_array($result2);
+
 	mysqli_query($db,"insert into tmp_paiement (code, date, transaction, tel, statut)
 					 values('$message','$date','$transaction', '$numero', '$status')");
 	
@@ -21,7 +27,7 @@ if ($row > 0) {
 
 	$post = [
 		'tel_marchand' => '074814529',
-		'montant' => $row->montant,
+		'montant' => $get_infos['montant'],
 		'ref'   => $transaction,
 		'tel_client'  => $numero,
 		'token'   => $token,
@@ -39,10 +45,7 @@ if ($row > 0) {
 	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 	$resultat = curl_exec($ch);
-
-
-	var_dump($resultat);
-
+   echo 'success';
 }else{
 	echo 'code invalide';
 }
